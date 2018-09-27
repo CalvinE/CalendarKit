4318f27ae95c2cb79f39e9f6325c1a79849d6bf1
calvin echols <calvin@calvin-mbp.localdomain>
2018-09-27 13:32:39 -0400

    Initial Commit

---
+++ CalendarKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarKit
+//
+//  Created by calvin echols on 9/27/18.
+//  Copyright © 2018 Strong Link Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
